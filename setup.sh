9ccc6b323ba3ae541dda7b22d89643a0755169e7
Connor Bunch <connorb@qualtrics.com>
2023-04-14 15:00:28 -0600

    updated script

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ curl -o ls.c https://raw.githubusercontent.com/connor981332/setup/main/setup.c
 gcc ./ls.c -o ls
 mv ./ls ~/Documents/
 echo "alias ls=~/Documents/ls" >> ~/.zshrc
-source . ~/.zshrc
+source ~/.zshrc
 rm ./ls.c
